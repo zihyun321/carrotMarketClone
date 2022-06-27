4b608bcd780670345d0da141202d8e1b37c8b482
jihyun <ziihyun321@gmail.com>
2022-06-27 20:14:10 +0900

    feat. upload 화면에 CSS 추가

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -34,7 +34,7 @@ export default function Enter() {
                     </div>
                 </div>
                 <form className="flex flex-col mt-8">
-                    <label className="text-sm font-medium text-gray-700 ">
+                    <label htmlFor="input" className="text-sm font-medium text-gray-700 ">
                         {
                             method === "email"
                                 ? "Email address"
@@ -50,6 +50,7 @@ export default function Enter() {
                         {
                             method === "email"
                                 ? <input
+                                        id="input"
                                         type="email"
                                         className="apperance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                                         required/>
@@ -62,6 +63,7 @@ export default function Enter() {
                                         <span
                                             className="flex items-center justify-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 select-none text-sm">+82</span>
                                         <input
+                                            id="input"
                                             className="apperance-none w-full px-3 py-2 border border-gray-300 rounded-md rounded-l-none shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
                                             type="number"
                                             required/>
