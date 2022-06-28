753a9c13a928b3a132d9ded721b7080f184e2864
jihyun <ziihyun321@gmail.com>
2022-06-28 16:47:27 +0900

    feat. Name input 추가 및 htmlFor 추가

--- pages/items/upload.tsx
+++ pages/items/upload.tsx
@@ -3,7 +3,7 @@ from "next";
 
 const Upload: NextPage = () => {
     return (
-        <div className="px-4 py-10">
+        <div className="px-4 space-y-5 py-10">
             <div>
                 <label
                     className="w-full text-gray-600 hover:text-orange-500 hover:border-orange-500 flex items-center justify-center border-2 border-dashed border-gray-300 py-6 h-48 rounded-md">
@@ -19,12 +19,28 @@ const Upload: NextPage = () => {
                             strokeLinecap="round"
                             strokeLinejoin="round"/>
                     </svg>
-
                     <input className="hidden" type="file"/>
                 </label>
             </div>
-            <div className="my-5">
-                <label htmlFor="price" className="mb-1 block text-sm font-medium text-gray-700">Price</label>
+
+            <div>
+                <label
+                className="mb-1 block text-sm font-medium text-gray-700"
+                htmlFor="name"
+                >
+                Name
+                </label>
+                <div className="rounded-md relative flex  items-center shadow-sm">
+                <input
+                    id="name"
+                    type="email"
+                    className="appearance-none w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-orange-500 focus:border-orange-500"
+                    required
+                />
+                </div>
+            </div>
+            <div>
+                 <label htmlFor="price" className="mb-1 block text-sm font-medium text-gray-700">Price</label>
                 <div className="rounded-md relative flex items-center shadow-sm">
                     <div className="absolute left-0 pointer-events-none pl-3 flex items-center justify-center">
                         <span className="text-gray-500 text-sm">$</span>
@@ -40,10 +56,12 @@ const Upload: NextPage = () => {
                 </div>
             </div>
             <div>
-                <label className="mb-1 block text-sm font-medium text-gray-700">Description</label>
-                <textarea className="mt-1 shadow-sm w-full focus:ring-2 focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500" rows={4}/>
+                <label htmlFor="description" className="mb-1 block text-sm font-medium text-gray-700">Description</label>
+                <textarea id="description" className="mt-1 shadow-sm w-full focus:ring-2 focus:ring-orange-500 rounded-md border-gray-300 focus:border-orange-500" rows={4}/>
             </div>
-            <button className="mt-4 w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none">Upload product</button>
+            <button className=" w-full bg-orange-500 hover:bg-orange-600 text-white py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 focus:outline-none ">
+                Upload item
+            </button>
         </div>
     );
 };
