b6535a34b7b80b8d416478c88742c9314c8ebaf4
jihyun <ziihyun321@gmail.com>
2022-06-28 14:34:42 +0900

    feat. Chat Detail 화면 CSS 추가

--- pages/chats/[id].tsx
+++ pages/chats/[id].tsx
@@ -1,30 +1,104 @@
-import type { NextPage } from "next";
+import type {NextPage}
+from "next";
 
 const ChatDetail: NextPage = () => {
-  return (
-    <div>
-      <div>
-        <div />
-        <div>Hi how much are you selling them for?</div>
-      </div>
-      <div>
-        <div />
-        <div>I want ￦20,000</div>
-      </div>
-      <div>
-        <div />
-        <div>미쳤어</div>
-      </div>
-      <div>
-        <div>
-          <input type="text" />
-          <div>
-            <span>&rarr;</span>
-          </div>
+    return (
+        <div className="py-10 px-4 space-y-4">
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div
+                    className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">Hi how much are you selling them for?</div>
+            </div>
+            <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>I want ￦20,000</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>미쳤어</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div
+                    className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">Hi how much are you selling them for?</div>
+            </div>
+            <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>I want ￦20,000</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>미쳤어</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div
+                    className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">Hi how much are you selling them for?</div>
+            </div>
+            <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>I want ￦20,000</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>미쳤어</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div
+                    className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">Hi how much are you selling them for?</div>
+            </div>
+            <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>I want ￦20,000</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>미쳤어</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div
+                    className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">Hi how much are you selling them for?</div>
+            </div>
+            <div className="flex flex-row-reverse items-start space-x-2 space-x-reverse">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>I want ￦20,000</p>
+                </div>
+            </div>
+            <div className="flex items-start space-x-2">
+                <div className="w-8 h-8 rounded-full bg-slate-400"/>
+                <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-200 rounded-md">
+                    <p>미쳤어</p>
+                </div>
+            </div>
+            <div className="fixed w-full mx-auto max-w-md bottom-2 inset-x-0">
+                <div className="flex relative items-center">
+                    <input type="text" className="shadow-sm rounded-full w-full border-gray-300 focus:ring-orange-300 focus:outline-none focus:border-orange-500 pr-12"/>
+                    <div className="absolute inset-y-0 flex py-1.5 pr-1.5 right-0">
+                        <button className="flex focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 items-center bg-orange-500 rounded-full px-3 hover:bg-orange-600 text-sm text-white">&rarr;</button>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default ChatDetail;
