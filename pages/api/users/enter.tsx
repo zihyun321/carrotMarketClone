4e13c39c874db5a854cf2d15cfd72b3641a89ccb
jihyun <ziihyun321@gmail.com>
2022-07-23 19:04:02 +0900

    feat. DB User 확인 or 생성로직을 upsert로 변경 및 if else 구문 변경하기

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -7,47 +7,19 @@ async function handler(
   res: NextApiResponse
 ) {
   const { phone, email } = req.body;
-  let user;
-  if (email) {
-    user = await client.user.findUnique({
-      where: {
-        email: email  // === email,
-      }
-    });
-    if (user) {
-      console.log("Found it!");
-    }
-    if (!user) {
-      console.log("Did not found. will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          email,
-        }
-      })
-    }
-    console.log(user);
-  }
-  if (phone) {
-    user = await client.user.findUnique({
-      where: {
-        phone: +phone,  // + 붙여야 string에서 number로 변환된다. (만약 num -> string 경우는 number + "")
-      }
-    });
-    if (user) {
-      console.log("Found it!");
-    }
-    if (!user) {
-      console.log("Did not found. will create");
-      user = await client.user.create({
-        data: {
-          name: "Anonymous",
-          phone: +phone,
-        }
-      })
-    }
-    console.log(user);
-  }
+  const user = await client.user.upsert({
+    where: {
+      ...(phone && { phone: +phone }),  // ...()은 if else와 같은 로직
+      ...(email && { email }),
+    },
+    create: {
+      name: "Anonymous",  // create는 name이 필수값이다.
+      ...(phone && { phone: +phone }),
+      ...(email && { email }),
+    },
+    update: {}
+  });
+  console.log(user);
   return res.status(200).end();
 }
 
