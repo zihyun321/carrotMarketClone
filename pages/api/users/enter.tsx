980e43e5222cc8edca7d227a0ea8bf08d09e63a6
jihyun <ziihyun321@gmail.com>
2022-07-23 18:54:52 +0900

    feat. DB에서 email/phone 가진 User 확인 or 생성하기

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -6,7 +6,48 @@ async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  let user;
+  if (email) {
+    user = await client.user.findUnique({
+      where: {
+        email: email  // === email,
+      }
+    });
+    if (user) {
+      console.log("Found it!");
+    }
+    if (!user) {
+      console.log("Did not found. will create");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          email,
+        }
+      })
+    }
+    console.log(user);
+  }
+  if (phone) {
+    user = await client.user.findUnique({
+      where: {
+        phone: +phone,  // + 붙여야 string에서 number로 변환된다. (만약 num -> string 경우는 number + "")
+      }
+    });
+    if (user) {
+      console.log("Found it!");
+    }
+    if (!user) {
+      console.log("Did not found. will create");
+      user = await client.user.create({
+        data: {
+          name: "Anonymous",
+          phone: +phone,
+        }
+      })
+    }
+    console.log(user);
+  }
   return res.status(200).end();
 }
 
